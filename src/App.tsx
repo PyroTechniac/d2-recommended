afcd6e5765ebec128de2ccbce8a4896eadb640d8
Gryffon Bellish <owenbellish@gmail.com>
2023-08-15 15:35:40 -0400

    remove stats.html

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { type FC } from 'react';
-import Manifest from './components/Manifest';
+// import Manifest from './components/Manifest';
 import Loader from './components/Loader';
 
 export const App: FC = () => {
